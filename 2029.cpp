66b08561d102883c75e4f57d6c2c5b675a784865
harsh chahal <harshchahal7889@gmail.com>
2026-08-16 21:48:11 +0530

    Add new solutions

---
+++ 2029.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    class Solution
+    {
+    public:
+        bool stoneGameIX(vector<int> &stones)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            for (int i : stones)
+            {
+                if (i % 3 == 0)
+                {
+                    a++;
+                }
+                else if (i % 3 == 1)
+                {
+                    b++;
+                }
+                else
+                {
+                    c++;
+                }
+            }
+
+            if (a % 2 == 0)
+            {
+                return b > 0 && c > 0;
+            }
+
+            return abs(b - c) > 2;
+        }
+    };
+}
